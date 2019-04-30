cd45763a6f5d3d9059441515a88baf5edfbb3513
Michael Reichert <michael.reichert@geofabrik.de>
2019-04-30 11:06:06 +0200

    bugfix: use final tower node IDs, not the preliminary node IDs

--- src/main/java/de/geofabrik/osmi_routing/NoExitHook.java
+++ src/main/java/de/geofabrik/osmi_routing/NoExitHook.java
@@ -1,28 +1,32 @@
 package de.geofabrik.osmi_routing;
 
+import com.carrotsearch.hppc.LongScatterSet;
+import com.carrotsearch.hppc.LongSet;
 import com.graphhopper.reader.ReaderNode;
 import com.graphhopper.reader.osm.OSMReader;
 import com.graphhopper.reader.osm.OSMReaderHook;
 
-public class NoExitHook implements OSMReaderHook {
-    
-    private OSMReader reader;
-    private OsmIdAndNoExitStore nodeInfo;
+public class NoExitHook extends OSMReaderHook {
 
-    public NoExitHook(OSMReader reader, OsmIdAndNoExitStore nodeInfo) {
-        this.reader = reader;
+    private OsmIdAndNoExitStore nodeInfo;
+    private LongSet noExitNodes;
+
+    public NoExitHook(OsmIdAndNoExitStore nodeInfo) {
         this.nodeInfo = nodeInfo;
+        this.noExitNodes = new LongScatterSet();
     }
-    
+
     public void processNode(ReaderNode node) {
-        int internalNodeId;
-        try {
-            internalNodeId = reader.getNodeMap().get(node.getId());
-        } catch (NullPointerException e) {
-            return;
+        if (node.hasTag("noexit", "yes")) {
+            noExitNodes.add(node.getId());
         }
-        if (internalNodeId >= 0) {
-            nodeInfo.addNodeInfo(internalNodeId, node.getId(), node.hasTag("noexit", "yes"));
+    }
+
+    public void addTowerNode(long osmId, double lat, double lon, double ele, int towerId) {
+        // Ensure that artifically created OSM IDs don't mess things up. They are a hack in
+        // GraphHopper when handling barriers which split an edge.
+        if (osmId > 0) {
+            nodeInfo.addNodeInfo(OSMReader.towerIdToMapId(towerId), osmId, noExitNodes.contains(osmId));
         }
     }
 }
