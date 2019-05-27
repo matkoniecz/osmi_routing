97b4e72dcf874902e4bb99bd4c8df394bbaa5a60
Michael Reichert <michael.reichert@geofabrik.de>
2019-05-27 16:21:32 +0200

    remove unnecessary build flag

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 BASEDIR=$(pwd)
 rm -rf maven_repository/*
 cd graphhopper
-mvn --projects api,core,reader-osm -P include-client-hc -am -DskipTests=true compile package
+mvn --projects api,core,reader-osm -am -DskipTests=true compile package
 mvn deploy:deploy-file -Durl=file://$(pwd)/../maven_repository/ -Dfile=core/target/graphhopper-core-0.12-SNAPSHOT.jar -DgroupId=com.graphhopper -DartifactId=graphhopper-core -Dpackaging=jar -Dversion=0.12.0-SNAPSHOT
 mvn deploy:deploy-file -Durl=file://$(pwd)/../maven_repository/ -Dfile=reader-osm/target/graphhopper-reader-osm-0.12-SNAPSHOT.jar -DgroupId=com.graphhopper -DartifactId=graphhopper-reader-osm -Dpackaging=jar -Dversion=0.12.0-SNAPSHOT
 mvn deploy:deploy-file -Durl=file://$(pwd)/../maven_repository/ -Dfile=api/target/graphhopper-api-0.12-SNAPSHOT.jar -DgroupId=com.graphhopper -DartifactId=graphhopper-api -Dpackaging=jar -Dversion=0.12.0-SNAPSHOT
